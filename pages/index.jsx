0b0da33ad29407a08d7f0ae257e1ba027b08372b
Carol Evangelista <cdsevangelista@inf.ufpel.edu.br>
2021-11-14 12:56:54 -0300

    Changing perspective camera and other parameters on the scene

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { useEffect } from "react";
-import SceneInit from "../SceneInit";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Stats from "three/examples/jsm/libs/stats.module";
 
 export default function Home() {
   let gui;
@@ -11,9 +12,43 @@ export default function Home() {
   };
 
   useEffect(async () => {
-    let scene = new SceneInit();
-    scene.initScene();
-    scene.animate();
+    const camera = new THREE.PerspectiveCamera(
+      36,
+      window.innerWidth / window.innerHeight,
+      1,
+      1000
+    );
+    
+    camera.position.z = 128;
+  
+    const scene = new THREE.Scene();
+  
+    const renderer = new THREE.WebGLRenderer({
+      canvas: document.getElementById("solar-system"),
+      antialias: true,
+    });
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+  
+    const controls = new OrbitControls(camera, renderer.domElement);
+  
+    const stats = Stats();
+    document.body.appendChild(stats.dom);
+  
+    window.addEventListener("resize", () => {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    }, false);
+    
+    function animateScene() {
+      window.requestAnimationFrame(animateScene.bind(this));
+      renderer.render(scene, camera);
+      stats.update();
+    }
+
+    animateScene();
 
     const sunGeometry = new THREE.SphereGeometry(14);
     const sunTexture = new THREE.TextureLoader().load("blue-sun-texture.jpg");
@@ -21,7 +56,7 @@ export default function Home() {
     const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial);
     const solarSystem = new THREE.Group();
     solarSystem.add(sunMesh);
-    scene.scene.add(solarSystem);
+    scene.add(solarSystem);
 
     const earthGeometry = new THREE.SphereGeometry(4);
     const earthTexture = new THREE.TextureLoader().load("earth-texture.jpg");
@@ -41,7 +76,7 @@ export default function Home() {
     const animate = () => {
       sunMesh.rotation.y += 0.001;
       earthMesh.rotation.y += 0.005;
-      earthSystem.rotation.y += 2 * Math.PI * (1 / 60) * (1 / 60);;
+      earthSystem.rotation.y += 2 * Math.PI * (1 / 60) * (1 / 60);//earth rotate around the sun in 1 minute
       requestAnimationFrame(animate);
     };
     animate();
