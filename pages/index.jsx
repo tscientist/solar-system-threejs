7aff5fb07f9d4d11edb458d33d86ad767f5d5ea1
Carol Evangelista <cdsevangelista@inf.ufpel.edu.br>
2021-11-14 12:04:57 -0300

    Creating earth object w/out rotation and adding it to solar system group

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,13 +15,24 @@ export default function Home() {
     scene.initScene();
     scene.animate();
 
-    const sunGeometry = new THREE.SphereGeometry(8);
+    const sunGeometry = new THREE.SphereGeometry(14);
     const sunTexture = new THREE.TextureLoader().load("blue-sun-texture.jpg");
     const sunMaterial = new THREE.MeshBasicMaterial({ map: sunTexture });
     const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial);
     const solarSystem = new THREE.Group();
     solarSystem.add(sunMesh);
     scene.scene.add(solarSystem);
+
+    const earthGeometry = new THREE.SphereGeometry(4);
+    const earthTexture = new THREE.TextureLoader().load("earth-texture.jpg");
+    const earthMaterial = new THREE.MeshBasicMaterial({ map: earthTexture });
+    const earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
+    earthMesh.position.x += 48;
+    
+    let earthSystem = new THREE.Group();
+    earthSystem.add(earthMesh);
+
+    solarSystem.add(earthSystem);
 
     await initGui();
     const solarSystemGui = gui.addFolder("solar system");
